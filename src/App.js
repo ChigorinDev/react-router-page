274f45c4b2e7b3b9bb0ece9be24b31c7e77442e4
Chigorin M.L <mmchigorin@gmail.com>
2019-04-17 21:38:12 +0300

    get rid of the trash and add materialize cdn link

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <button>123</button>
+      </div>
+    );
+  }
+}
+
+export default App;
