1e213947d7880986f5e518ee9e13d65f764a39a5
Chigorin M.L <mmchigorin@gmail.com>
2019-04-17 22:31:43 +0300

    add several pages and rotes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import NavBar from "./components/NavBar";
+import { BrowserRouter, Route } from "react-router-dom";
+import Home from './components/Home';
+import About from './components/About';
+import Contacts from './components/Contacts';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <button>123</button>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <NavBar />
+          <Route exact path='/' component={Home} />
+          <Route path='/about' component={About} />
+          <Route path='/contacts' component={Contacts} />
+        </div>
+      </BrowserRouter>
     );
   }
 }
